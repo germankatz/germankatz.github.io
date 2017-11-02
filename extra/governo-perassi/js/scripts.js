d4b7035a7d351f04bcc73f03c737ca1a17a89ab5
German <germanka@outlook.com>
2017-11-02 10:28:07 -0300

    Fixes and act. G&P

--- extra/governo-perassi/js/scripts.js
+++ extra/governo-perassi/js/scripts.js
@@ -8,3 +8,24 @@ $(document).ready(function(){
 	});	
 });
 	
+var fixmeTop = $('.home-nav').offset().top;    // get initial position of the element
+
+$(window).scroll(function() {                  // assign scroll event listener
+
+    var currentScroll = $(window).scrollTop(); // get current position
+
+    if (currentScroll >= fixmeTop) {           // apply position: fixed if you
+        $('.home-nav').css({                      // scroll to that element or below it
+            position: 'fixed',
+            top: '0',
+            left: '0'
+        });
+        $('#home-pubs').css("margin-top", "75px");
+    } else {                                   // apply position: static
+        $('.home-nav').css({                      // if you scroll above it
+            position: 'static'
+        });
+        $('#home-pubs').css("margin-top", "0px");
+    }
+
+});
